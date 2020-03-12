bcf3528f0049b099d5d9d3c8b7403711ee3c3ce9
idefumi <2000fumito@gmail.com>
2020-03-12 14:43:32 +0000

    scroll commit

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -4,24 +4,42 @@ $(function() {
 		$header = $('#header-class'),
 		$nav = $('nav'),
 		$boxheight = 'height';
-	$win.on('load scroll', function() {
-	  var value = $(this).scrollTop();
-		  height = $header.height();
-	  if ( value >= height ) {
-		$nav.addClass($boxheight);
-	  } else {
-		$nav.removeClass($boxheight);
-	  }
-	});
-  });
-  //ページ内繊維のスムーズ遷移
-  $(function(){
+		$win.on('load scroll', function() {
+			var value = $(this).scrollTop();
+			height = $header.height();
+		  	if ( value >= height ) {
+				$nav.addClass($boxheight);
+	  		} else {
+				$nav.removeClass($boxheight);
+			}
+		});
+});
+//ページ内繊維のスムーズ遷移
+$(function(){
 	$('a[href^="#"]').click(function(){
-	  var speed = 750;
-	  var href= $(this).attr("href");
-	  var target = $(href == "#" || href == "" ? 'html' : href);
-	  var position = target.offset().top;
-	  $("html, body").animate({scrollTop:position}, speed, "swing");
-	  return false;
+		var speed = 750;
+		var href= $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top;
+		$("html, body").animate({scrollTop:position}, speed, "swing");
+		return false;
 	});
-  });
+});
+//background-image変更
+$(function(){
+	var $win = $(window),
+		$header = $('header'),
+		$header_before  = 'header-img',
+		$header_before_change = 'header-img-change';
+		$win.on('load scroll', function(){
+			var value = $(this).scrollTop();
+			height = $header.height()/2;
+			if (value <= height){
+				$header.addClass($header_before);
+				$header.removeClass($header_before_change);
+			} else {
+				$header.addClass($header_before_change);
+				$header.removeClass($header_before);
+			}
+		});
+});
