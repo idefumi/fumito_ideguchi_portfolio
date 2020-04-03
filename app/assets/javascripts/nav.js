7f35aba505b133c1b3b7c0cf1ce419b2d838d017
idefumi <2000fumito@gmail.com>
2020-04-03 16:57:10 +0000

    ver3.0 commit

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -6,7 +6,7 @@ $(function() {
 		$boxheight = 'height';
 		$win.on('load scroll', function() {
 			var value = $(this).scrollTop();
-			height = $header.height()/2;
+			height = $header.height();
 		  	if ( value >= height ) {
 				$nav.addClass($boxheight);
 	  		} else {
@@ -25,21 +25,21 @@ $(function(){
 		return false;
 	});
 });
-//background-image変更
-$(function(){
-	var $win = $(window),
-		$header = $('header'),
-		$header_before  = 'header-img',
-		$header_before_change = 'header-img-change';
-		$win.on('load scroll', function(){
-			var value = $(this).scrollTop();
-			height = $header.height()/2;
-			if (value <= height){
-				$header.addClass($header_before);
-				$header.removeClass($header_before_change);
-			} else {
-				$header.addClass($header_before_change);
-				$header.removeClass($header_before);
-			}
-		});
-});
+// //background-image変更
+// $(function(){
+// 	var $win = $(window),
+// 		$header = $('header'),
+// 		$header_before  = 'header-img',
+// 		$header_before_change = 'header-img-change';
+// 		$win.on('load scroll', function(){
+// 			var value = $(this).scrollTop();
+// 			height = $header.height()/2;
+// 			if (value < height){
+// 				$header.addClass($header_before);
+// 				$header.removeClass($header_before_change);
+// 			} else {
+// 				$header.addClass($header_before_change);
+// 				$header.removeClass($header_before);
+// 			}
+// 		});
+// });
